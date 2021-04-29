8e9de2681353e0329c3beb426bae70f4a84f973a
Tim Vigneau <tjvig94@gmail.com>
2021-04-29 12:51:37 -0400

    Initial setup

---
+++ employee-directory/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from './components/Directory';
+import Searchbar from './components/Searchbar';
+
+function App() {
+  return (
+    <div>
+      <Searchbar />
+      <Directory />
+    </div>
+  );
+}
+
+export default App;
---
+++ employee-directory/src/components/Directory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Directory() {
+    return(
+        <h2>Directory</h2>
+    )
+};
+
+export default Directory;
